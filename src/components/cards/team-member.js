6502e5530402dfb564f9ecd55630d90c175ab02b
alsriva <alsriva@microsoft.com>
2021-09-07 00:00:44 +0530

    UI SKU

--- src/components/cards/team-member.js
+++ src/components/cards/team-member.js
@@ -2,16 +2,45 @@
 /** @jsx jsx */
 import { jsx, Flex, Box, Image, Text, Heading, Link } from 'theme-ui';
 import { FaTwitter, FaGithub, FaDribbble } from 'react-icons/fa';
+import { Button, Radio, Label } from '@theme-ui/components';
+
 
 const TeamMember = ({ member }) => {
   return (
-    <Box sx={styles.section}>
+    <Box style={{borderStyle: 'double'}} sx={styles.section}>
       <Flex as="figure" sx={styles.avatar}>
-        <Image src={member?.avatar} alt={member?.name} />
+        {/* <Image src={member?.avatar} alt={member?.name} /> */}
+        <div style={{width:380, height:420 , borderStyle: 'double'}}>
+          <div style={{display:'flex'}}>
+          <Image style={{width:180, height:200 , borderStyle: 'double'}}src={member?.avatar}></Image>
+          <div style={{ borderStyle: 'double' , width: '50%'}}> 
+            <ol>
+              skill 1
+            </ol>
+            <ol>
+              skill 2
+            </ol>
+            <ol>
+              skill 3
+            </ol>
+            <ol>
+              skill 4
+            </ol>
+          </div></div>
+          <div style={{ borderStyle: 'double' , width: '100%' , height: '50%'}}>
+            My acheivements bla bla
+          </div>
+        </div>
+
       </Flex>
-      <Box sx={styles.about}>
+      <Box style={{borderStyle: 'double'}} sx={styles.about}>
+        <div style={{display: 'flex' , justifyContent: 'space-between'}}>
+        <div>
         <Heading as="h3">{member?.name}</Heading>
         <Text as="p">{member?.designation}</Text>
+        </div>
+        <Button>Meet Me</Button>
+        </div>
         <Box sx={styles.socialLinks}>
           {member?.socialLinks?.map((social, index) => (
             <Link href={social?.link} key={index}>
